cad54b87fbe87ecd412e58ffa53f3480bd3da12d
nicolasamoroso <108936163+nicolasamoroso@users.noreply.github.com>
2024-04-03 18:01:40 -0300

    add react router and fetching api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react";
+import fetchMovies from "./api/movie-list";
+import { Movie } from "./types/movies";
+import TopMovies from "@/components/top-movies";
+import Billboard from "@/components/billboard";
+import Review from "@/components/review";
 import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchMovies();
+        setMovies(data.movies);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching films:", error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const top3Movies = movies.sort((a, b) => b.rating - a.rating).slice(0, 3);
+
   return (
-    <>
-      <p>Hola</p>
-    </>
+    <div style={{ position: "relative" }}>
+      <TopMovies movies={top3Movies} />
+      <Billboard movies={movies} />
+      <Review movies={movies} />
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NavBar from "@/components/nav/navbar.tsx";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import ThanksScreen from "@/components/thanks-screen/thanks-screen.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <NavBar />
+        <App />
+      </>
+    ),
+  },
+  {
+    path: "/thanks",
+    element: <ThanksScreen />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
